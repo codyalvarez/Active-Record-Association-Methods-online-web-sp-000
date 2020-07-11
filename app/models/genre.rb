8c664e0c4b92b93cbb344a784127defc07db8525
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-11 22:55:34 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -13,7 +13,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    self.artists.to_a
+    self.artists.to_s
     # return an array of strings containing every musician's name
   end
 end
